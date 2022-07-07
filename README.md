12a545f961cdfaab2abd203f74df903558d4d11b
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-07 02:42:30 -0300

    importa css básico e coloca rotas login e signUp no app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # projeto14-rocket-store-front
+
+## Fontes disponíveis:
+
+    font-family: 'Bangers', cursive;
+
+    font-family: 'Dancing Script', cursive;
+
+    font-family: 'Noto Sans JP', sans-serif;
+
+    font-family: 'Press Start 2P', cursive;
+
+    font-family: 'Roboto', sans-serif;
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import '../css/reset.css';
+import '../css/globalStyle.css';
+import SignUp from './Pages/SignUp';
+import Login from './Pages/Login.js';
+
 export default function App(){
 
 return (
-    <h1>Oi! Eu sou o Goku!</h1>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+    </BrowserRouter>
 )
 
 }
